7d234461d4d8ab78e844bf594ca345021f0a7d92
yuuki_takahara <takahara.yuuki22@gmail.com>
2021-10-17 09:10:54 +0900

    General error: 1709 対策方法1 push

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,8 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // if (config('app.force_ssl')) {
-        //     \URL::forceScheme('https');
-        // }
+        Schema::defaultStringLength(191);
     }
 }
