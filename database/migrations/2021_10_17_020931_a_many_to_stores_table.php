dfedb05d55693f6675a23a7f12826f8b7c1c8740
yuuki_takahara <takahara.yuuki22@gmail.com>
2021-10-21 09:35:39 +0900

    10/19まで　migrationファイル修正

--- database/migrations/2021_10_17_020931_a_many_to_stores_table.php
+++ database/migrations/2021_10_17_020931_a_many_to_stores_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddManyToStoresTable extends Migration
+class AManyToStoresTable extends Migration
 {
     /**
      * Run the migrations.
