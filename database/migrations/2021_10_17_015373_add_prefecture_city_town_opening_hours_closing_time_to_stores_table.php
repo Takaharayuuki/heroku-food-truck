ab7cbfe3b98716c43a8c779ada3920c5756e631c
yuuki_takahara <takahara.yuuki22@gmail.com>
2021-10-21 09:42:08 +0900

    Cannot declare class DManyToStoresManyTable, because the name is already in use

--- database/migrations/2021_10_17_015373_add_prefecture_city_town_opening_hours_closing_time_to_stores_table.php
+++ database/migrations/2021_10_17_015373_add_prefecture_city_town_opening_hours_closing_time_to_stores_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class DManyToStoresManyTable extends Migration
+class AddPrefectureCityTownOpeningHoursClosingTimeToStoresTable extends Migration
 {
     /**
      * Run the migrations.
