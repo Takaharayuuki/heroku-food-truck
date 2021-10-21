889c331760edcbd0babc0e8bf018e9151eb087f0
yuuki_takahara <takahara.yuuki22@gmail.com>
2021-10-21 09:38:38 +0900

    10/19まで　migrationファイル修正

--- database/migrations/2021_10_17_015373_add_prefecture_city_town_opening_hours_closing_time_to_stores_table.php
+++ database/migrations/2021_10_17_015373_add_prefecture_city_town_opening_hours_closing_time_to_stores_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddManyToStoresManyTable extends Migration
+class DManyToStoresManyTable extends Migration
 {
     /**
      * Run the migrations.
